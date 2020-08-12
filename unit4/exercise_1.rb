89b88f7c776102ebc586a3427ea518b48ff4827b
Kareha Agesa <kmaagesa@gmail.com>
2020-08-12 16:57:47 -0700

    finishing up exercises

--- unit4/exercise_1.rb
+++ unit4/exercise_1.rb
@@ -9,19 +9,61 @@
 # Read in the proposed Password to a variable (done)
 # If the password contains a number and a symbol (@, %, *, or !) and contains 8 or more characters (done)
 # then accept the password and congratulate the user, if not make them reenter the password until they enter an acceptable password (done)
-# [Optional] Make the user enter the password a second time to verify, and if it's not the same as the original password, make them repeat the process
-# [Optional] Make them include a capital and a lowercase letter
+# [Optional] Make the user enter the password a second time to verify, and if it's not the same as the original password, make them repeat the process (attempted)
+# [Optional] Make them include a capital and a lowercase letter (done)
 # [Optional] Let them know what they did wrong (done)
 
 puts "Please enter a password"
+puts "Password requirements: \n 1. Needs a number \n 2. Needs a symbol (@, %, *, or !) \n 3. Needs to contain 8 or more characters \n 4. Needs at least one capital letter \n 5. Needs at least one uppercase letter"
 password = gets.chomp
+
+# Goal: Make the user enter the password a second time to verify,
+# and if it's not the same as the original password, make them repeat the process
+# Code: Not sure if this accomplishes that, this just penalizes the user forever until they match..
+puts "Please re-enter your password"
+validate_password = gets.chomp
+while password != validate_password
+    puts "Passwords must match, please enter the same password"
+    validate_password = gets.chomp
+end
+
+# verify numbers, symbols, and password length
 numbers =  password.count("0-9")
 symbols =  password.include?("@") || password.include?("%") || password.include?("*") || password.include?("!")
 length = password.length >= 8 
-puts !numbers
 
+# function to verify if password contains capital letter
+def verify_capital(password)
+    capital = false
+    uppercase_letters = ('A'..'Z')
+    password.split("").each do |letter|
+        if uppercase_letters.include?(letter)
+            # I'm not sure if this violates Boolean Zen
+            capital = true
+        end
+    end
+    return capital
+end
 
-if numbers && symbols && length
+# function to verify if password contains lowercase letter
+def verify_lowercase(password)
+    lowercase_letters = ('a'..'z')
+    lowercase = false
+    password.split("").each do |letter|
+        if lowercase_letters.include?(letter)
+            # I'm not sure if this violates Boolean Zen
+            lowercase = true
+        end
+    end
+    return lowercase
+end
+
+# verify capital and lower case letter in password
+capital = verify_capital(password)
+lowercase = verify_lowercase(password)
+
+# interaction with user to validate password
+if numbers && symbols && length && capital && lowercase
     puts "Congratulations this was an acceptable password"
 else
     while numbers == 0
@@ -39,4 +81,26 @@ else
         second_password = gets.chomp
         length = second_password.length >= 8 
     end
+    capital = verify_capital(password)
+    while !capital
+        puts "Please enter a password with a capital letter"
+        second_password = gets.chomp
+        capital = verify_capital(second_password)
+    end
+    lowercase = verify_lowercase(password)
+    while !lowercase
+        puts "PLease enter a password with a lowercase letter"
+        second_password = gets.chomp
+        lowercase = verify_capital(second_password)
+    end
+    puts "Congratulations this was an acceptable password"
 end
+
+# an improvement to ponder.. 
+# right now all conditions in the above if statement are based on the initial password
+# and dont update dymanically,
+# so say the user enters a password that fails numbers and symbols
+# theyll first hit an error with numbers
+# but if they fix numbers and symbols at that point, only the
+# numbers fix will be taken into account..
+# (because the symbols check is based on their initial password)
--- unit4/exercise_2.rb
+++ unit4/exercise_2.rb
@@ -10,39 +10,64 @@
 # Optional Enhancements
 # Some of these enhancements are better solved after having knowledge from Lesson 12: Arrays, so you can try some and then come back and do others after Lesson 12.
 
-# Handle ties for a winner appropriately
+# Handle ties for a winner appropriately (done)
 # Handle grammar of vote summary saying vote or votes appropriately (done)
-# Handle write in votes
+# Handle write in votes (done) 
 # Consider how to handle more than 10 votes (done)
-# Consider how to handle more than 3 candidates
+# Consider how to handle more than 3 candidates 
 # Refactor your code so that your code is DRY (Don't Repeat Yourself) (done)
 
-puts "Welcome to my election voting program \n Election candidates are Sue, John, and Kevin \nHow many people will be voting?"
-constituents = get.chomp.to_i
+puts "Welcome to my election voting program \nElection candidates are Sue, John, and Kevin \nHow many people will be voting?"
+constituents = gets.chomp.to_i
 
+valid_candidates = ["sue", "john", "kevin"]
+
+# retain the chosen candidate for each user
 votes = []
 constituents.times do |vote|
     puts "Please input your vote"
-    candidate = gets.chomp
+    candidate = gets.chomp.downcase
+    # ensure chosen candidate is not a write in
+    if !valid_candidates.include?(candidate)
+        puts "Please enter a candidate mentioned above"
+        candidate = gets.chomp
+    end
     puts "Vote #{vote + 1}: #{candidate}"
     votes << candidate
 end
 
-vote_hash = {Sue: votes.count("Sue"),
-            John: votes.count("John"),
-            Kevin: votes.count("Kevin")
+# create a hash with the candidates and number of votes they got
+vote_hash = {Sue: votes.count("sue"),
+            John: votes.count("john"),
+            Kevin: votes.count("kevin")
 }
 
-["Sue", "John", "Kevin"].each do |person|
-    votes = vote_hash[:"#{person}"]
+# output number of votes each candidate got
+valid_candidates.each do |person|
+    votes = vote_hash[:"#{person.capitalize}"]
     if votes == 1
         string = "vote"
     else
         string = "votes"
     end
-    puts "#{person} - #{vote_hash[:"#{person}"]} #{string}"
+    puts "#{person} - #{votes} #{string}"
 end
 
+# account for ties
+# retain the highest number of votes
+max_value = vote_hash.values.max
 
+# if a candidate got this high number of votes - consider them a winner
+winners = []
+vote_hash.each do |key, value|
+    if value == max_value
+        winners << key.to_s
+    end
+end
 
-puts "The winner is #{vote_hash.key(vote_hash.values.max).to_s}"
+# print summary sentence with the winner
+if winners.length > 1
+    puts "The winners are #{winners.join(", ")}"
+else
+    puts "The winner is #{winners.join(", ")}"
+end
