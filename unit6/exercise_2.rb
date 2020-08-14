5ad1a13ee7bc6e4ae646aac2dce40f8e87950c92
Kareha Agesa <kmaagesa@gmail.com>
2020-08-13 22:22:45 -0700

    exercise 2

--- unit6/exercise_2.rb
+++ unit6/exercise_2.rb
@@ -15,3 +15,37 @@
 
 # 1. Utilize a single array variable to store all student information,
 # instead of three individual arrays. This array will contain many hashes.
+
+
+# 2. Utilize a single loop to drive the hash population
+# (you may have nested loops inside this loop for other functionality):
+# accept user input for the student's name
+# generate random student ID
+# generate student email address from previous pieces of data
+
+# 3. Update the printing functionality to utilize this new hash variable
+# to print out student roster
+
+student_info = []
+
+5.times do |index|
+    puts "Please enter a student's first and last name"
+    # get name
+    name = gets.chomp.upcase
+    # generate random id number
+    id_number = rand(111111..999999)
+    # goal: get first initials
+    # first step: capture everything but last name
+    first_names = name.split(" ")[0..-2]
+    # then get first initials
+    initial = first_names.map {|i| i[0]}
+    # get last name
+    last_name = name.split(" ")[-1]
+    # get last 3 digits of student id
+    digits = id_number.to_s[-3..-1]
+    # create email address
+    email = "#{initial.join("")}#{last_name}#{digits}@adadevelopersacademy.org"
+    student_info[index] = {name: name, id_number: id_number, email: email}
+end
+
+student_info.each {|i| puts "Student: #{i[:name]} ID Number: #{i[:id_number]} Email: #{i[:email]} \n"}
